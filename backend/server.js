d25e2bec116d44c46c5a132a5ce15e39c18b9de7
Rashmika K R <krrashmika2004@gmail.com>
2025-06-29 22:12:13 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,44 @@
 // app.js
-const express = require("express");
-const connectDB = require("./dbConnection"); // Import the database connection function
-const cors = require("cors"); // Import the CORS middleware
-const bodyParser = require("body-parser");
+const express = require('express');
+const connectDB = require('./dbConnection'); // Import the database connection function
+const cors = require('cors'); // Import the CORS middleware
+const bodyParser = require("body-parser");  
 const app = express();
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
+
 
 // Connect to the database
 connectDB();
-require("dotenv").config(); // Load environment variables from .env file
+require('dotenv').config(); // Load environment variables from .env file
 // Middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Allow requests only from the frontend origin
-    methods: "GET,POST,DELETE,PUT", // Specify allowed methods
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"], // Explicitly allow these headers
-    // Allow cookies to be sent
-  })
-);
-// Enable CORS for all routes
+// CORS configuration
+const corsOptions = {
+  //origin: 'https://ai-attendance-client.vercel.app', // Your client URL
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+  credentials: true, // Allow cookies and credentials
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+};
+
+// Apply CORS middleware globally
+app.use(cors(corsOptions));
+ // Enable CORS for all routes
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.json());
 // Routes
-const studentRoutes = require("./routes/studentRoutes");
-const attendanceRoutes = require("./routes/attendanceRoutes");
-const reportRoutes = require("./routes/reportRoutes");
-const authRoutes = require("./routes/authRoutes");
-const uploadCsvRoutes = require("./routes/uploadCsv"); // Import the CSV upload route
-const deleteStudentsRoutes = require("./routes/deleteStudents"); // Import the delete students route
-// Use the CSV upload route
+const studentRoutes = require('./routes/studentRoutes');
+const attendanceRoutes = require('./routes/attendanceRoutes');
+const reportRoutes = require('./routes/reportRoutes');
+const authRoutes = require('./routes/authRoutes');
+
 //const excelReportRoutes = require('./routes/excelReportRoutes');
 
-app.use("/api/students", studentRoutes);
-app.use("/api/attendance", attendanceRoutes);
-app.use("/api/report", reportRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/upload", uploadCsvRoutes);
-app.use("/api/delete-students", deleteStudentsRoutes); // Use the delete students route
+app.use('/api/students', studentRoutes);
+app.use('/api/attendance', attendanceRoutes);
+app.use('/api/report', reportRoutes);
+app.use('/api/auth', authRoutes);
+
 //app.use('/api/excel', excelReportRoutes);
 
 // Start the server
