3ed4b1f6c6db5c715e612c58c2614912a96cf135
jaisanth123 <jaisanth2006@gmail.com>
2025-07-14 11:37:23 +0530

    sidebar updated

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ require("dotenv").config(); // Load environment variables from .env file
 // CORS configuration
 const corsOptions = {
   origin: [
+    
     "https://attendance-kec.onrender.com",
     "http://localhost:5173",
   ].filter(Boolean), // Remove any undefined values
