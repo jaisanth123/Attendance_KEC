d5732661449bed382833229c3deec8e5b033310e
jaisanth123 <jaisanth2006@gmail.com>
2025-07-15 07:44:51 +0530

    backend cors

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -2,8 +2,8 @@
 const mongoose = require("mongoose");
 
 // MongoDB URI from MongoDB Atlas
-const uri =
-  "mongodb+srv://hodcse:Z3EZVI40t7lpr23S@cluster0.dm26ae7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://hodcse:Z3EZVI40t7lpr23S@cluster0.dm26ae7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  // "mongodb+srv://hodcse:Z3EZVI40t7lpr23S@cluster0.dm26ae7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // "mongodb+srv://hodcse:Z3EZVI40t7lpr23S@cluster0.dm26ae7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // Connect to MongoDB
 const connectDB = async () => {
